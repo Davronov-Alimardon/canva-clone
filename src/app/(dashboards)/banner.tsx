e944790f758bd64dc20b1e9a244d0a53cc751554
= <emailaddr>
2024-07-12 21:12:05 +0500

    added link for banner button

--- src/app/(dashboards)/banner.tsx
+++ src/app/(dashboards)/banner.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { ArrowRight, Sparkles } from "lucide-react";
+import { ArrowRight, Loader2, Sparkles } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useState } from "react";
 
 export const Banner = () => {
+  const [loading, setLoading] = useState(false);
   return (
     <div className="text-white aspect-[5/1] min-h-[248px] flex gap-x-6 p-6 items-center rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5]">
       <div className="rounded-full size-28 items-center justify-center bg-white/50 hidden md:flex">
@@ -17,10 +20,16 @@ export const Banner = () => {
         <p className="text-xs md:text-sm mb-2">
           Turn inspiration into design in no time. Simply upload an image and let AI do the rest.
         </p>
-        <Button variant="secondary" className="w-[160px]">
-          Start creating
-          <ArrowRight className="size-4 ml-2" />
-        </Button>
+        <Link href="/editor/beta" onClick={() => setLoading(true)}>
+          <Button variant="secondary" disabled={loading} className="w-[160px]">
+            Start creating
+            {loading ? (
+              <Loader2 className="size-4 ml-2 animate-spin" />
+            ) : (
+              <ArrowRight className="size-4 ml-2" />
+            )}
+          </Button>
+        </Link>
       </div>
     </div>
   );
