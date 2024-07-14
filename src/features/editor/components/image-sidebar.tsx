f767979995ae97049cf1599848c446336ad7fdae
= <emailaddr>
2024-07-14 11:56:45 +0500

    image sidebar issue

--- src/features/editor/components/image-sidebar.tsx
+++ src/features/editor/components/image-sidebar.tsx
@@ -2,10 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { AlertTriangle, Loader, Upload } from "lucide-react";
 
-import { 
-  ActiveTool, 
-  Editor,
-} from "@/features/editor/types";
+import { ActiveTool, Editor } from "@/features/editor/types";
 import { ToolSidebarClose } from "@/features/editor/components/tool-sidebar-close";
 import { ToolSidebarHeader } from "@/features/editor/components/tool-sidebar-header";
 
@@ -19,13 +16,9 @@ interface ImageSidebarProps {
   editor: Editor | undefined;
   activeTool: ActiveTool;
   onChangeActiveTool: (tool: ActiveTool) => void;
-};
+}
 
-export const ImageSidebar = ({
-  editor,
-  activeTool,
-  onChangeActiveTool,
-}: ImageSidebarProps) => {
+export const ImageSidebar = ({ editor, activeTool, onChangeActiveTool }: ImageSidebarProps) => {
   const { data, isLoading, isError } = useGetImages();
 
   const onClose = () => {
@@ -36,21 +29,18 @@ export const ImageSidebar = ({
     <aside
       className={cn(
         "bg-white relative border-r z-[40] w-[360px] h-full flex flex-col",
-        activeTool === "images" ? "visible" : "hidden",
+        activeTool === "images" ? "visible" : "hidden"
       )}
     >
-      <ToolSidebarHeader
-        title="Images"
-        description="Add images to your canvas"
-      />
+      <ToolSidebarHeader title="Images" description="Add images to your canvas" />
       <div className="p-4 border-b">
         <UploadButton
           appearance={{
             button: "w-full text-sm font-medium",
-            allowedContent: "hidden"
+            allowedContent: "hidden",
           }}
           content={{
-            button: "Upload Image"
+            button: "Upload Image",
           }}
           endpoint="imageUploader"
           onClientUploadComplete={(res) => {
@@ -66,37 +56,37 @@ export const ImageSidebar = ({
       {isError && (
         <div className="flex flex-col gap-y-4 items-center justify-center flex-1">
           <AlertTriangle className="size-4 text-muted-foreground" />
-          <p className="text-muted-foreground text-xs">
-            Failed to fetch images
-          </p>
+          <p className="text-muted-foreground text-xs">Failed to fetch images</p>
         </div>
       )}
       <ScrollArea>
         <div className="p-4">
           <div className="grid grid-cols-2 gap-4">
-            {data && data.map((image) => {
-              return (
-                <button
-                  onClick={() => editor?.addImage(image.urls.regular)}
-                  key={image.id}
-                  className="relative w-full h-[100px] group hover:opacity-75 transition bg-muted rounded-sm overflow-hidden border"
-                >
-                  <Image
-                    fill
-                    src={image.urls.small}
-                    alt={image.alt_description || "Image"}
-                    className="object-cover"
-                  />
-                  <Link
-                    target="_blank"
-                    href={image.links.html}
-                    className="opacity-0 group-hover:opacity-100 absolute left-0 bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50 text-left"
+            {data &&
+              data.map((image) => {
+                return (
+                  <button
+                    onClick={() => editor?.addImage(image.urls.regular)}
+                    key={image.id}
+                    className="relative w-full h-[100px] group hover:opacity-75 transition bg-muted rounded-sm overflow-hidden border"
                   >
-                    {image.user.name}
-                  </Link>
-                </button>
-              )
-            })}
+                    <Image
+                      fill
+                      src={image?.urls?.small || image?.urls?.thumb}
+                      alt={image?.alt_description || "Image"}
+                      className="object-cover"
+                      loading="lazy"
+                    />
+                    <Link
+                      target="_blank"
+                      href={image.links.html}
+                      className="opacity-0 group-hover:opacity-100 absolute left-0 bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50 text-left"
+                    >
+                      {image.user.name}
+                    </Link>
+                  </button>
+                );
+              })}
           </div>
         </div>
       </ScrollArea>
